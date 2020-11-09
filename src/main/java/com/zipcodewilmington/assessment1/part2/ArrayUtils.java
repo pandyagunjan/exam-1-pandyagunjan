90737ccf86e568cdb0aa4ea29884bf7a3961dc9b
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-09 12:21:12 -0500

    Multiplier done

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -134,5 +134,7 @@ public class ArrayUtils {
         }
 
         return storeAltered;
+
+
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -48,7 +48,16 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        ArrayList<Integer> tracker = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++) {
+            if(ints[i]%3!=0)
+            {
+                tracker.add(ints[i]);
+            }
+
+        }
+
+        return tracker.toArray(new Integer[0]);
     }
 
     /**
@@ -58,6 +67,15 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+         ArrayList<Integer> tracker = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++) {
+            if(ints[i]%multiple!=0)
+            {
+                tracker.add(ints[i]);
+            }
+
+        }
+
+        return tracker.toArray(new Integer[0]);
     }
 }
